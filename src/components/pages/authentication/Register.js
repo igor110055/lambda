f5652737dc423a87b66e83af7daddf638eef4b49
ccmeyers-dev <charlotteccmeyers@outlook.com>
2022-01-23 06:03:42 +0100

    replaced authentication pages axios with axiosInstance to pass headers also merged two commits using rebase s is a cherry-pick from quantcastfinance to amextradefx
    amend to axios work a5b6d5e

--- src/components/pages/authentication/Register.js
+++ src/components/pages/authentication/Register.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import storage from "local-storage-fallback";
 import { useHistory, useLocation } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -46,11 +45,7 @@ const Register = () => {
       formData.referrer = state.referrer;
     }
     try {
-      const { data } = await axios.post("/api/auth/register", formData, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const { data } = await axiosInstance.post("/auth/register", formData);
 
       axiosInstance.defaults.headers["Authorization"] =
         "Bearer " + data.accessToken;
